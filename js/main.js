eeb6de8457f0afb53f1e7155c10f2329d5241d4c
ahtrag <garthaprasidhiyanta@gmail.com>
2018-12-25 23:58:39 +0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/**
+ * ==============================
+ * Your Javascript Code Goes Here
+ * ==============================
+ **/
+
+'use strict';
+/* global Monogatari */
+/* global monogatari */
+
+const { $_ready } = Monogatari;
+// const {$_opening} = Ocean;
+
+// $_opening (() => {
+// 	ocean.init('#ocean');
+// 	$_()
+// })
+
+$_ready (() => {
+	monogatari.init ('#monogatari');
+	$_('#monogatari [data-menu="main"]').prepend ('<h1></h1>');
+	$_('#monogatari [data-ui="inner-menu"]').toggleClass ('vertical horizontal');
+	$_('#monogatari [data-ui="inner-menu"]').toggleClass ('vertical--right horizontal--center');
+});
