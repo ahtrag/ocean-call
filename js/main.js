6e6ebf372df542bea8d5bb1263a28b92bbc7c9f9
ahtrag <garthaprasidhiyanta@gmail.com>
2019-01-06 01:04:23 +0700

    edit styling in main

--- js/main.js
+++ js/main.js
@@ -18,7 +18,7 @@ const { $_ready } = Monogatari;
 
 $_ready (() => {
 	monogatari.init ('#monogatari');
-	$_('#monogatari [data-menu="main"]').prepend ('<h1 style="color: #00acc1;">Ocean Calls</h1>');
+	$_('#monogatari [data-menu="main"]');
 	$_('#monogatari [data-ui="inner-menu"]').toggleClass ('vertical horizontal');
 	$_('#monogatari [data-ui="inner-menu"]').toggleClass ('vertical--right horizontal--center');
 });
